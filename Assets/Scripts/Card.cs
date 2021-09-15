24f40d3c77b812acd9f50020e32d45fe8c74210a
ApotoxinElish <19079181d@connect.polyu.hk>
2021-09-15 23:20:26 +0800

    Update

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -1,18 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Card : MonoBehaviour
+public class Card
 {
-    // Start is called before the first frame update
-    void Start()
+    public int id;
+    public string cardName;
+
+    // 构造函数
+    public Card(int _id, string _cardName)
     {
-        
+        this.id = _id;
+        this.cardName = _cardName;
+    }
+}
+
+public class MonsterCard : Card
+{
+    public int attack;
+    public int healthPoint;
+    public int healthPointMax;
+    // 等级、属性
+
+    public MonsterCard(int _id, string _cardName, int _attack, int _healthPoint, int _healthPointMax) : base(_id, _cardName)
+    {
+        this.attack = _attack;
+        this.healthPoint = _healthPoint;
+        this.healthPointMax = _healthPointMax;
     }
 
-    // Update is called once per frame
-    void Update()
+}
+
+public class SpellCard : Card
+{
+    public string effect;
+
+    public SpellCard(int _id, string _cardName, string _effect) : base(_id, _cardName)
     {
-        
+        this.effect = _effect;
     }
 }
