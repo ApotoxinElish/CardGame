565471b07bdad031d577876e69d4fca210be1588
ApotoxinElish <19079181d@connect.polyu.hk>
2022-01-05 01:14:16 +0800

    Update

---
+++ Assets/Scripts/CardDisplay.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CardDisplay : MonoBehaviour
+{
+
+    public TMP_Text nameText;
+    public TMP_Text attackText;
+    public TMP_Text healthText;
+    public TMP_Text effectText;
+
+    public Image backgroundImage;
+
+    public Card card;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ShowCard();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ShowCard()
+    {
+        nameText.text = card.cardName;
+        if (card is MonsterCard)
+        {
+            var monster = card as MonsterCard;
+            attackText.text = monster.attack.ToString();
+            healthText.text = monster.healthPoint.ToString();
+
+            effectText.gameObject.SetActive(false);
+        }
+        else if (card is SpellCard)
+        {
+            var spell = card as SpellCard;
+            effectText.text = spell.effect;
+
+            attackText.gameObject.SetActive(false);
+            healthText.gameObject.SetActive(false);
+        }
+    }
+}
