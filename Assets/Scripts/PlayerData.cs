1476602b87a3736c1c5c4f0bfb50a34304a87e08
ApotoxinElish <19079181d@connect.polyu.hk>
2022-01-13 14:18:03 +0800

    Update PlayerData.cs

--- Assets/Scripts/PlayerData.cs
+++ Assets/Scripts/PlayerData.cs
@@ -13,7 +13,7 @@ public class PlayerData : MonoBehaviour
     public TextAsset playerData;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         CardStore.LoadCardData();
         LoadPlayerData();
