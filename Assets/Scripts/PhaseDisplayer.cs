5ea2782bdab726197277b299cbc3061966ae2630
ApotoxinElish <19079181d@connect.polyu.hk>
2022-01-29 20:56:02 +0800

    Update

---
+++ Assets/Scripts/PhaseDisplayer.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PhaseDisplayer : MonoBehaviour
+{
+    public TMP_Text phaseText;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        BattleManager.Instance.phaseChangeEvent.AddListener(UpdateText);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void UpdateText()
+    {
+        phaseText.text = BattleManager.Instance.GamePhase.ToString();
+    }
+}
